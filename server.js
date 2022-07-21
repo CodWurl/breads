d9bdfbebc9a136b95885b86be18d8494ff08b540
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-07-21 19:18:16 -0400

    Added 404 page

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 
-// ROUTES
+// ROUTE
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
 })
@@ -22,7 +23,7 @@ app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
 
-// ROUTES
+// ROUTE
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
@@ -30,4 +31,10 @@ app.get('/', (req, res) => {
   // Breads
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
+
+  // 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
   
