44ddcfbd69db232e3b7eb6b1211d94b848d80d2f
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-07-23 14:08:24 -0400

    added 404 page reconfigured

---
+++ views/error404.jsx
@@ -0,0 +1,13 @@
+const React = require ('react')
+//Adding 404 page view
+function error404 () {
+return (
+    <Default>
+        <head>
+            <h1>404 : PAGE NOT FOUND</h1>
+            <p>Oops, sorry, we can't find this page!</p>
+        </head>
+    </Default>
+)
+}
+module.exports = error404
