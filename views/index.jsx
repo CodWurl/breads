84593e4758beca1017d18a006571c725fc7b1b05
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-07-24 22:35:45 -0400

    create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -6,21 +6,26 @@ const Default = require('./layouts/Default')
 function Index({ breads, title }) {
     return (
         <Default title={title}>
-            
-            
+
+
             <h2>Index Page</h2>
             {/*<p>I have {breads[0].name} bread!</p>*/}
+
+            <div className="newButton">
+                <a href="/breads/new"><button>Add a new bread</button></a>
+            </div>
+
 
             <ul>
                 {
                     breads.map((bread, index) => {
                         return (<li key={index}>
                             <a href={`/breads/${index}`}>
-                            {bread.name}
-                            </a> 
+                                {bread.name}
+                            </a>
                         </li>
                         )
-                           
+
                     })
                 }
             </ul>
