2e20331de3c01fcb3a906dd9b14643a3857b7a4e
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-08-01 21:41:52 -0400

    added breads schema

--- server.js
+++ server.js
@@ -41,4 +41,7 @@ app.get('*', (req, res) => {
   res.render('error404')
 })
 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
   
